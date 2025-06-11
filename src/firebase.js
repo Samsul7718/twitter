c22ffbbffd876889cf6b48b7a46277f2fdd5e056
Samsul Alam <samsulalam@Samsuls-MacBook-Air.local>
2025-06-11 12:43:27 +0530

    firebase update

--- src/firebase.js
+++ src/firebase.js
@@ -1,28 +1,4 @@
-// import firebase from "firebase/app";
 
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth"; // Optional, depending on usage
-// Add other Firebase services as needed
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBwRqvU5v8FRspmnffIbGP50gh1Z4c-Nbw",
-//   authDomain: "twitter-app-8b4bc.firebaseapp.com",
-//   projectId: "twitter-app-8b4bc",
-//   storageBucket: "twitter-app-8b4bc.firebasestorage.app",
-//   messagingSenderId: "929836876045",
-//   appId: "1:929836876045:web:4766367a57a9e994db3044",
-// };
-// const firebaseApp = firebase.initializeApp(firebaseConfig);
-
-// const db = firebaseApp.firestore();
-// export default db;
-
-
-// const db = initializeApp(firebaseConfig);
-// const auth = getAuth(app); // Optional
-
-// export default db;
-// export { db }; // Export what you need
 
 
 // firebase.js
